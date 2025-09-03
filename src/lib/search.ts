a2443764831db38006a31a654a7b5d1767e6b5c6
yspv <yusupovamirxon@yahoo.com>
2025-09-03 07:07:00 +0500

    chore: remove exposed API key from code

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -1,31 +1,12 @@
-import TypesenseInstantSearchAdapter from "typesense-instantsearch-adapter";
 import Typesense from "typesense";
 
 export const typesense = new Typesense.Client({
-  apiKey: "bx7D48M8rNYNez0OdyAaLpHE62c2jhGR",
+  apiKey: process.env.TYPESENSE_KEY!,
   nodes: [
     {
-      host: "4r9awqhjftzn5c6yp-1.a1.typesense.net",
+      host: process.env.TYPESENSE_URL!,
       port: 443,
       protocol: "https",
     },
   ],
 });
-
-const adapter = new TypesenseInstantSearchAdapter({
-  server: {
-    apiKey: "bx7D48M8rNYNez0OdyAaLpHE62c2jhGR",
-    nodes: [
-      {
-        host: "4r9awqhjftzn5c6yp-1.a1.typesense.net",
-        port: 443,
-        protocol: "https",
-      },
-    ],
-  },
-  additionalSearchParameters: {
-    query_by: "name",
-  },
-});
-
-export const searchClient = adapter.searchClient;
